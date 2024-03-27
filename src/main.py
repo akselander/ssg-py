db03601cd5d27a4d5e48b8c72b55716aa675ddb9
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2024-03-27 23:51:03 +0100

    chore: copy static files to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
+import os
+import shutil
 from textnode import TextNode
 
 
+def copy(source, dest):
+    if os.path.isfile(source):
+        print(f"Copy from {source} to {dest}")
+        shutil.copy(source, dest)
+        return
+
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for file in os.listdir(source):
+        from_path = os.path.join(source, file)
+        to_path = os.path.join(dest, file)
+        copy(from_path, to_path)
+
+
+path_static = "static"
+path_public = "public"
+
+
 def main():
-    print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
+    if os.path.exists(path_public):
+        print(f"Deleting {path_public}")
+        shutil.rmtree(path_public)
+    print(f"Copying files from {path_static} to {path_public}...")
+    copy("static", "public")
 
 
 main()
